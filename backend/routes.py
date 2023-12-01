5173d2a8fa3243215e925e39e4027434b16ba1cd
God-Serenaa <aarnabofficial@gmail.com>
2023-12-01 20:41:26 +0600

    templates added

--- backend/routes.py
+++ backend/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask , request, render_template
 from controllers import result
 app = Flask(__name__)
 
@@ -6,11 +6,15 @@ app = Flask(__name__)
 def root():
     return "hello from arnab"
 
-@app.route('/api/predict', methods=['POST'])
-def predictRoute():
-    request_data = request.get_json()
-
-    return result(request_data)
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    if request.method == "GET":
+        return render_template('utils/input.html')
+    if request.method == "POST":
+        request_data = request.get_json()
+        print(request_data)
+        response = result(request_data)
+        return render_template('utils/result.html', value=response)
 
 
 
